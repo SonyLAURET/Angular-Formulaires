f717b38d60fae165155a4813399ba77d46d828cf
Sony LAURET <sony.lauret@gmail.com>
2018-06-15 15:57:42 +0200

    validator crée pour mail et password

---
+++ angular-forms/src/app/validator/validators.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import {isPrefixNartawakValidator} from "./password.validator";
+@Component({
+  selector: 'app-validators',
+  templateUrl: './validators.component.html',
+  styleUrls: ['./validators.component.css']
+})
+export class ValidatorsComponent implements OnInit {
+  emailCtrl: FormControl;
+  passwordCtrl: FormControl;
+  userForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+
+    // Création des contrôles
+    this.emailCtrl = fb.control('', [Validators.email, Validators.required]); 
+    this.passwordCtrl = fb.control('',[isPrefixNartawakValidator]);
+   
+    // Création du groupe (aka le formulaire)
+    this.userForm = fb.group({
+      email: this.emailCtrl,
+      password: this.passwordCtrl
+    });
+  }
+  handleClear() {
+    this.emailCtrl.setValue('');
+    this.passwordCtrl.setValue('');
+  }
+  handleSubmit() {
+    console.log(this.userForm.value);
+  }
+  ngOnInit() {
+  }
+}
